9bd4082c39f6941672e904058401ee5431ae02ea
lajuman <108601538+lajuman@users.noreply.github.com>
2024-04-14 14:36:15 +0700

    Create main.py

---
+++ Polygon Area Calculator/main.py
@@ -0,0 +1,54 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        picture = ''
+        for i in range(self.height):
+            picture += '*' * self.width + '\n'
+        return picture
+
+    def get_amount_inside(self, shape):
+        width_fit = self.width // shape.width
+        height_fit = self.height // shape.height
+        return width_fit * height_fit
+
+    # if an instance of a Rectangle is represented as a string, it should look like: Rectangle(width=5, height=10)
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def set_width(self, width):
+        self.set_side(width)
+
+    def set_height(self, height):
+        self.set_side(height)
+
+    def __str__(self):
+        return f"Square(side={self.width})"
